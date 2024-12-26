0fa9456faf883807611f107c1166cc5629ae9bd0
runyu <wangrunyu010@gmail.com>
2024-12-26 19:38:17 +0800

    upgrade to angular 18

--- cds-frontend/src/app/app.module.ts
+++ cds-frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -12,57 +12,49 @@ import { TaskListComponent } from './task-list/task-list.component';
 
 import { NgxEditorModule } from 'ngx-editor';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    CreateTaskComponent,
-    UpdateTaskComponent,
-    TaskDetailsComponent,
-    TaskListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    NgxEditorModule.forRoot({
-      locals: {
-        // menu
-        bold: 'Bold',
-        italic: 'Italic',
-        code: 'Code',
-        blockquote: 'Blockquote',
-        underline: 'Underline',
-        strike: 'Strike',
-        bullet_list: 'Bullet List',
-        ordered_list: 'Ordered List',
-        heading: 'Heading',
-        h1: 'Header 1',
-        h2: 'Header 2',
-        h3: 'Header 3',
-        h4: 'Header 4',
-        h5: 'Header 5',
-        h6: 'Header 6',
-        align_left: 'Left Align',
-        align_center: 'Center Align',
-        align_right: 'Right Align',
-        align_justify: 'Justify',
-        text_color: 'Text Color',
-        background_color: 'Background Color',
-
-        // popups, forms, others...
-        url: 'URL',
-        text: 'Text',
-        openInNewTab: 'Open in new tab',
-        insert: 'Insert',
-        altText: 'Alt Text',
-        title: 'Title',
-        remove: 'Remove',
-        enterValidUrl: 'Please enter a valid URL',
-      },
-    }),
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        CreateTaskComponent,
+        UpdateTaskComponent,
+        TaskDetailsComponent,
+        TaskListComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        NgxEditorModule.forRoot({
+            locals: {
+                // menu
+                bold: 'Bold',
+                italic: 'Italic',
+                code: 'Code',
+                blockquote: 'Blockquote',
+                underline: 'Underline',
+                strike: 'Strike',
+                bullet_list: 'Bullet List',
+                ordered_list: 'Ordered List',
+                heading: 'Heading',
+                h1: 'Header 1',
+                h2: 'Header 2',
+                h3: 'Header 3',
+                h4: 'Header 4',
+                h5: 'Header 5',
+                h6: 'Header 6',
+                align_left: 'Left Align',
+                align_center: 'Center Align',
+                align_right: 'Right Align',
+                align_justify: 'Justify',
+                text_color: 'Text Color',
+                background_color: 'Background Color',
+                // popups, forms, others...
+                url: 'URL',
+                text: 'Text',
+                openInNewTab: 'Open in new tab',
+                insert: 'Insert',
+                altText: 'Alt Text',
+                title: 'Title',
+                remove: 'Remove',
+                enterValidUrl: 'Please enter a valid URL',
+            },
+        })], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
