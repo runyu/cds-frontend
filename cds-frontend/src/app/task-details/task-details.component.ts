e2317bae199faecda63d1c114a25c321f380e7ba
runyu <wangrunyu010@gmail.com>
2024-12-26 21:51:34 +0800

    add editor sample content

--- cds-frontend/src/app/task-details/task-details.component.ts
+++ cds-frontend/src/app/task-details/task-details.component.ts
@@ -5,6 +5,8 @@ import { TaskService } from '../task.service';
 
 import { Editor, Toolbar } from 'ngx-editor';
 
+import jsonDoc from './doc';
+
 @Component({
   selector: 'app-task-details',
   templateUrl: './task-details.component.html',
@@ -15,30 +17,29 @@ export class TaskDetailsComponent implements OnInit {
   id!: number
   task: Task = new Task();
   editor!: Editor;
-  toolbar!: Toolbar;
-  html = '';
+  toolbar: Toolbar = [
+    ['bold', 'italic'],
+    ['underline', 'strike'],
+    ['code', 'blockquote'],
+    ['ordered_list', 'bullet_list'],
+    [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
+    ['link', 'image'],
+    ['text_color', 'background_color'],
+    ['align_left', 'align_center', 'align_right', 'align_justify'],
+  ];
+
+  editorDoc=jsonDoc;
 
   constructor(private route: ActivatedRoute, private taskService: TaskService) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
-
     this.task = new Task();
     this.taskService.getTaskById(this.id).subscribe( data => {
       this.task = data;
     });
-
+    
     this.editor = new Editor();
-    this.toolbar = [
-      ['bold', 'italic'],
-      ['underline', 'strike'],
-      ['code', 'blockquote'],
-      ['ordered_list', 'bullet_list'],
-      [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
-      ['link', 'image'],
-      ['text_color', 'background_color'],
-      ['align_left', 'align_center', 'align_right', 'align_justify'],
-    ];
   }
 
   // make sure to destory the editor
